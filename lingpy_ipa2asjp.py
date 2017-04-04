8a7f1ba4256d31fe938de8c90976c8fea1134a25
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2017-04-04 11:34:24 +0200

    Split functionality into separatie functions

--- lingpy_ipa2asjp.py
+++ lingpy_ipa2asjp.py
@@ -8,25 +8,38 @@ import glob
 from lingpy import ipa2tokens
 
 
-def read_convert_ipa_asjp():
-    ipa2asjp_dict = defaultdict(str)
-    f = codecs.open(os.path.join(os.path.dirname(__file__),
-                                 "ipa2asjp.txt"),
-                    "r", encoding="utf-8")
+def read_ipa_to_asjp(
+        filename=os.path.join(os.path.dirname(__file__),
+                              "ipa2asjp.txt")):
+    """Read a SSV mapping IPA symbols to ASJP classes."""
+    ipa_to_asjp = {}
+
+    f = codecs.open(filename, "r", encoding="utf-8")
+
     for line in f:
         line = line.replace("\n", "")
         line = line.replace("\r", "")
-        temp = line.split(" ")
-        ipa = temp[0]
-        asjp = ""
-        if temp[1] == "":
-            asjp = "0"
-        else:
-            asjp = temp[1]
-        ipa2asjp_dict[ipa] = asjp
+        ipa, asjp = line.split(" ")
+        ipa_to_asjp[ipa] = asjp
     f.close()
+    return ipa_to_asjp
 
-    f_trace = codecs.open("test_ipa2asjp.txt", "w", encoding="utf-8")    
+
+ipa_to_asjp = read_ipa_to_asjp()
+
+
+def ipa2asjp(ipa):
+    """Convert an IPA string into a ASJP token string."""
+    tokenized_word = ipa2tokens(ipa)
+    asjp_list = [ipa_to_asjp[x] for x in tokenized_word]
+    asjp_word = ""
+    for k in asjp_list:
+        k = k.replace("0", "")
+        asjp_word += k
+
+
+def read_convert_ipa_asjp():
+    f_trace = codecs.open("test_ipa2asjp.txt", "w", encoding="utf-8")
     for file_name in glob.iglob("data/*tsv"):
         f_trace.write(file_name+"\n")
         f = codecs.open(file_name, "r", encoding="utf-8")
@@ -36,24 +49,14 @@ def read_convert_ipa_asjp():
         header = header.split("\t")
         for line in f:
             arr = line.split("\t")
-            lang, global_id, ipa_word, cog_class = arr[0], arr[3], arr[5], arr[6]
-            tokenized_word = ipa2tokens(ipa_word)
-            asjp_list = [ipa2asjp_dict[x] for x in ipa_word]
-            asjp_word = ""
-            for k in asjp_list:
-                k = k.replace("0", "")
-                if k == "0":
-                    f_trace.write(ipa_word+"\t" + "".join(asjp_list)+"\n")
-                    continue
-                else:
-                    asjp_word += k
-            arr[5] = asjp_word
+            arr[5] = ipa2asjp(arr[5])
             fout.write(
                 "\t".join(arr))
         f.close()
         fout.close()
     f_trace.close()
-    
+
+
 
 if __name__ == "__main__":
     read_convert_ipa_asjp()
