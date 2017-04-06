4c5aa2f914eaed2d2d97de5a3f2947294e917176
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2017-04-06 16:09:24 +0200

    Replace LD code with a needleman_wunsch call

--- distances.py
+++ distances.py
@@ -52,62 +52,16 @@ def normalized_leventsthein(a, b):
     la = la - 1;
     lb = lb - 1
     return float(m[la][lb])/ float(max(la, lb))
-    
-def LD(x,y,lodict=None):
+
+
+def LD(x, y, lodict={}):
+    """Standard NW alignment
+
+    Needleman-Wunsch algorithm for pairwise string alignment with
+    default gap penalties.
+
     """
-    x is target, y is source
-    Needleman-Wunsch algorithm for pairwise string alignment
-    with affine gap penalties.
-    'lodict' must be a dictionary with all symbol pairs as keys
-    and match scores as values.
-    gp1 and gep are gap penalties for opening/extending a gap.
-    Returns the alignment score and one optimal alignment.
-    """
-    n,m = len(x),len(y)
-    dp = np.zeros((n+1,m+1))
-    pointers = np.zeros((n+1,m+1),np.int32)
-    for i in range(1,n+1):
-        if not lodict:
-            dp[i,0] = dp[i-1,0]+1
-        else:
-            dp[i,0] = dp[i-1,0]+lodict['-',x[i-1]]
-        pointers[i,0]=1
-    for j in range(1,m+1):
-        if not lodict:
-            dp[0,j] = dp[0,j-1]+1
-        else:
-            dp[0,j] = dp[0,j-1]+lodict[y[j-1],'-']
-        pointers[0,j]=2
-    for i in range(1,n+1):
-        for j in range(1,m+1):
-            if not lodict:
-                match = dp[i-1,j-1]
-                if x[i-1] != y[j-1]:
-                    match = match+1                
-                insert = dp[i-1,j]+1
-                delet = dp[i,j-1]+1
-            else:
-                match = dp[i-1,j-1]+lodict[x[i-1],y[j-1]]
-                insert = dp[i-1,j]+lodict['-',x[i-1]]
-                delet = dp[i,j-1]+lodict[y[j-1],'-']
-            min_score = min([match,insert,delet])
-            dp[i,j] = min_score
-            pointers[i,j] = [match,insert,delet].index(min_score)
-    alg = []
-    i,j = n,m
-    while(i>0 or j>0):
-        pt = pointers[i,j]
-        if pt==0:
-            i-=1
-            j-=1
-            alg = [[x[i],y[j]]]+alg
-        if pt==1:
-            i-=1
-            alg = [[x[i],'-']]+alg
-        if pt==2:
-            j-=1
-            alg = [['-',y[j]]]+alg
-    return dp[-1,-1], alg
+    return needleman_wunsch(x, y, lodict=lodict, gop=-1, gep=-1)
 
 
 def needleman_wunsch(x, y, lodict={}, gop=-2.5, gep=-1.75):
